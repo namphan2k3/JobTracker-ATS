41e60ec3af74319658f83235f9ea81e27d266895
Nam <ptnam1672003@gmail.com>
2026-03-23 23:14:37 +0700

    fix(health-check): fix health check api

--- jobtracker-app/src/main/java/com/jobtracker/jobtracker_app/controllers/HealthController.java
+++ jobtracker-app/src/main/java/com/jobtracker/jobtracker_app/controllers/HealthController.java
@@ -1,26 +1,5 @@
 package com.jobtracker.jobtracker_app.controllers;
 
-import com.jobtracker.jobtracker_app.dto.responses.common.ApiResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-
-@RestController
-@RequestMapping("/public/health")
-public class HealthController {
-
-    @GetMapping
-    public ApiResponse<Map<String, String>> health() {
-        return ApiResponse.<Map<String, String>>builder()
-                .message("OK")
-                .data(Map.of("status", "UP"))
-                .build();
-    }
-}
-package com.jobtracker.jobtracker_app.controllers;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
